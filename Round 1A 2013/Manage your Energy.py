9dd86db717766897041860a37802cdbe07f94fc9
alibaransel <alibaransel25@gmail.com>
2023-04-08 19:34:31 +0300

    Trying new approach

--- Round 1A 2013/Manage your Energy.py
+++ Round 1A 2013/Manage your Energy.py
@@ -1,16 +1,35 @@
-
 def solve(e, r, n, vList):
-    vListSum = 0
-    vListMax = vList[0]
-    maxCount = 0
-    for v in vList:
-        vListSum += v
-        if v > vListMax:
-            vListMax = v
-            maxCount = 1
-        elif v == vList:
-            maxCount += 1
-    return vListSum * r + (e - r) * vListMax
+    distances = [0 for _ in range(n)]
+    oldVList = []
+    oldVIList = []
+    for vI in range(n):
+        v = vList[vI]
+        for i in range(len(oldVList) - 1, -1, -1):
+            oldV = oldVList[i]
+            if oldV < v:
+                oldVI = oldVIList.pop(i)
+                oldVList.pop(i)
+                distances[oldVI] = vI - oldVI
+            else:
+                break
+        oldVList.append(v)
+        oldVIList.append(vI)
+    currE = e
+    result = 0
+    for i in range(n):
+        v = vList[i]
+        d = distances[i]
+        if d == 0:
+            result += v * currE
+            currE = 0
+        else:
+            extraE = currE + d * r - e
+            if extraE > 0:
+                eSpend = min(currE, extraE)
+                currE -= eSpend
+                result += v * eSpend
+        currE += r
+    return result
 
 
 if __name__ == "__main__":
